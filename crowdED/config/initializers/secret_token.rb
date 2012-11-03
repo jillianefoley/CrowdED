a1789e83c65ce803e3c05d3385321ad2b74c2fe5
Tyler Martin <tmartin@tmartin-VirtualBox.(none)>
2012-11-03 15:48:51 -0400

    Initial crowdED application

---
+++ crowdED/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrowdED::Application.config.secret_token = '8a698fcb597c692069b2cb4f6df549495b886293550b415a97c0edc5ab0377f7fe9bb697a0fdd696827371b68963dc34dddaa1569b7f503e48611431c441d710'
